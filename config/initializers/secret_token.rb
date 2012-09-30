e21b6dd98faa07e2995b5bbd4508e9c69d5010d9
orkun <orkun@ubuntu.(none)>
2012-09-30 03:46:16 -0700

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharky::Application.config.secret_token = '3f4badd817074ba746f124dc414a78e290ac8d51a7da935ebf1d49f459b6ae564e8d74875651ee0744224b415269d2d20ffd23bcd864b7d19068c6423c9a8757'
